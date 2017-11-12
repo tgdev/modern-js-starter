5fa501c421d9b5ba8925342aff05d32329d86639
Tom Gillard <tomgillar@gmail.com>
2017-11-12 23:04:55 +1100

    Fixed eslint error.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'babel-polyfill';
 // Remove demo and console log to add more imports or js here...
 import welcome from './demo/demo';
+/* eslint-disable no-console */
 console.log(welcome('developer'));
